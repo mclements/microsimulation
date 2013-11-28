68f70ed378a42f00f3adf6342db6cd4316931077
aleja067 <alexandra.jauhiainen@ki.se>
2013-11-28 13:35:08 +0100

    Changes to match wrap output

--- R/calibSim.R
+++ R/calibSim.R
@@ -10,7 +10,9 @@ callCalibrationPerson <- function(seed=rep(12345,6),n=500,runpar=c(4,0.5,0.05,10
 #  curnames <- names(out)
   mat <- matrix(0,nr=10,ncol=length(states))
   colnames(mat) <- states; rownames(mat) <- seq(10,100,10)
-  mat[,]<-data.matrix(as.data.frame(out$Value[match(states,out$Key)]))
+  pick <- match(states,out$Key)
+  pick <- pick[!is.na(pick)]
+  mat[,states %in% out$Key]<-data.matrix(as.data.frame(out$Value[pick]))
 	
   list(StateOccupancy=mat, TimeAtRisk=out$Value[out$Key=="TimeAtRisk"][[1]])
 }
