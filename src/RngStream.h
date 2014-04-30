45ec8f1c06b840d7e79c5360b2df10334ba30dc8
Mark Clements <mark.clements@ki.se>
2014-04-30 09:20:07 +0200

    Revert changes to RngStream.*

--- src/RngStream.h
+++ src/RngStream.h
@@ -1,9 +1,30 @@
+/**
+ * @file    RngStream.h for multiple streams of Random Numbers
+ * @author  Pierre L'Ecuyer, University of Montreal
+ * Original date: 14 August 2001
+ * Modified by Mark Clements <mark.clements@ki.se> 2014-03-22 for the microsimulation package.
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+*/
 
  
 #ifndef RNGSTREAM_H
 #define RNGSTREAM_H
  
 #include <string>
+
+namespace ssim {
 
 class RngStream
 {
@@ -73,6 +94,8 @@ double U01d ();
 
 
 };
+
+} // ssim namespace
  
 #endif
  
