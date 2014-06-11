207b73a01c22ffd85a5cad1b7979f3a998d3b105
Alexandra Jauhiainen <alexandra.jauhiainen@ki.se>
2014-06-11 17:48:52 +0200

    Added simple parallelization for calibration

--- R/calibSim.R
+++ R/calibSim.R
@@ -1,5 +1,5 @@
 callCalibrationPerson <- function(seed=12345,n=500,runpar=c(4,0.5,0.05,10,3,0.5),mc.cores=1) {
-
+  
   state <- RNGstate(); on.exit(state$reset())
   RNGkind("user")
   set.user.Random.seed(seed)
@@ -8,6 +8,7 @@ callCalibrationPerson <- function(seed=12345,n=500,runpar=c(4,0.5,0.05,10,3,0.5)
   chunks <- split(1:n,f)
   initialSeeds <- list()
   currentSeed <- user.Random.seed()
+  
   fun <- function(obj, i) if(i==1)obj else parallel::nextRNGStream(obj)
   for(i in 1:mc.cores){
     initialSeeds[[i]] <- fun(currentSeed,i)
@@ -17,13 +18,14 @@ callCalibrationPerson <- function(seed=12345,n=500,runpar=c(4,0.5,0.05,10,3,0.5)
     chunk <- chunks[[i]]
     set.user.Random.seed(initialSeeds[[i]])
     .Call("callCalibrationSimulation",list(n=as.integer(length(chunk)),runpar=as.double(runpar)),PACKAGE="microsimulation")
-    })
-    out <- lapply(out, function(o){
-      curnames <- names(o)
-      mat <- matrix(0,nr=10,ncol=length(states))
-      colnames(mat) <- states; rownames(mat) <- seq(10,100,10)
-      mat[,states[states %in% curnames]]<-data.matrix(transform(as.data.frame(o[states[states %in% curnames]])))
-      list(StateOccupancy=mat, TimeAtRisk=o$TimeAtRisk)
-    })
-    Reduce(function(u,z) list(StateOccupancy=u$StateOccupancy + z$StateOccupancy, TimeAtRisk = u$TimeAtRisk + z$TimeAtRisk),out)
+  })
+  states <- c("DiseaseFree","Precursor","PreClinical","Clinical")
+  out <- lapply(out, function(o){
+    curnames <- names(o)
+    mat <- matrix(0,nr=10,ncol=length(states))
+    colnames(mat) <- states; rownames(mat) <- seq(10,100,10)
+    mat[,states[states %in% curnames]]<-data.matrix(transform(as.data.frame(o[states[states %in% curnames]])))
+    list(StateOccupancy=mat, TimeAtRisk=o$TimeAtRisk)
+  })
+  Reduce(function(u,z) list(StateOccupancy=u$StateOccupancy + z$StateOccupancy, TimeAtRisk = u$TimeAtRisk + z$TimeAtRisk),out)
 }
