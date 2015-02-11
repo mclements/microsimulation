c9d171bf4334bb477edcefed1ae4e96fa9c94632
andreasakarlsson <andreas.a.karlsson@gmail.com>
2015-02-11 19:08:24 +0100

    PDC report: Added profiling results from R-side

--- doc/Rprofiling.R
+++ doc/Rprofiling.R
@@ -1,4 +1,5 @@
-#  PROJECT 
+
+                                        #  PROJECT 
 #  =======
 #  
 #  Execution time profiling of the FHCRC model.
@@ -18,27 +19,33 @@ require(ggplot2)
 ## @knitr Profiling_FHCRC
 ## Profiling fhcrc implementation
 ## =============================================================================
-n <- 1e6
+n <- 1e7
 Rprof(tmp <- tempfile(),memory.profiling=T)
 out <- callFhcrc(n=n, screen="noScreening")
 
 Rprof()
-X11()
 prof <- summaryRprof(tmp)
 data <- cbind(prof$by.self, process=rownames(prof$by.self))
 data$process <- factor(data$process, levels=rownames(prof$by.self))
 data <- head(data,20)
 ggplot(data, aes(x =process, y = self.pct )) + geom_bar(stat="identity") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  ggtitle('By self process')
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    ggtitle('By self process')
+ggsave("selfProcess.pdf",
+    width = 10,
+    height = 8,
+    dpi = 1200)
 
-X11()
+
 data_tot <- cbind(prof$by.total, process=rownames(prof$by.total))
 data_tot$process <- factor(data_tot$process, levels=rownames(prof$by.total))
 data_tot_top <- head(data_tot,20)
 ggplot(data_tot_top, aes(x =process, y = total.pct , fill = self.pct/total.pct)) + geom_bar(stat="identity") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ggtitle('By parent process coloured by self portion')
-
+ggsave("parentColByPortion.pdf",
+    width = 10,
+    height = 8,
+    dpi = 1200)
 unlink(tmp)
 ## =============================================================================
