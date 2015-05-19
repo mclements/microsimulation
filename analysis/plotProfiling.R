5364741d2aeb634d27d63182834f8c1be268ae57
andreasakarlsson <andreas.a.karlsson@gmail.com>
2015-05-19 17:06:47 +0200

    Scaling of the compared implementations

---
+++ analysis/plotProfiling.R
@@ -0,0 +1,6 @@
+require(ggplot2)
+df <- read.csv("/home/andkar/src/ki/microsimulation/analysis/openMPprofiling.csv", header=T)
+
+ggplot(data = df, aes(x = cores, y = time)) + geom_line(aes(group = Setting, colour = Setting)) + xlab("No. of cores") + ylab("Time [s]") + ggtitle("Microsimulation of 'no screening' with 1e7 individuals") 
+
+ggsave("plotProfiling.pdf")
