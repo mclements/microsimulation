dc080bf7e01216355f975ed14f500ce47c9783c2
andreasakarlsson <andreas.a.karlsson@gmail.com>
2015-06-15 15:19:05 +0200

    Line to bar plot for consistency

--- analysis/plotProfiling.R
+++ analysis/plotProfiling.R
@@ -1,8 +1,11 @@
 require(ggplot2)
 df <- read.csv("./implementationProfiling.csv", header=T)
 df$Setting <- factor(df$Setting, levels(df$Setting)[c(3,2,1)])
-ggplot(data = df, aes(x = cores, y = time)) + geom_line(aes(group = Setting, colour = Setting)) + xlab("No. of cores") + ylab("Time [s]") + ggtitle("Microsimulation of 'no screening' with 1e7 individuals") +
-  scale_colour_hue(name = "Implementation")
+ggplot(data = df, aes(x = cores, y = time, fill=Setting)) +
+    geom_bar(stat="identity", position="dodge")+
+    xlab("No. of cores") + ylab("Time [s]") +
+    ggtitle("Microsimulation of 'no screening' with 1e7 individuals") +
+  scale_fill_hue(name = "Implementation")
 
 ggsave("../doc/report/images/implementationProfiling.pdf")
 
