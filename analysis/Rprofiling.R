7ef24fa470829611613630123d6d8cd79d160c1f
andreasakarlsson <andreas.a.karlsson@gmail.com>
2015-06-24 18:56:54 +0200

    Added multi-node text + minor changes

--- analysis/Rprofiling.R
+++ analysis/Rprofiling.R
@@ -38,13 +38,10 @@ ggsave("selfProcess.pdf",
 
 data_tot <- cbind(prof$by.total, process=rownames(prof$by.total))
 data_tot$process <- factor(data_tot$process, levels=rownames(prof$by.total))
-data_tot_top <- head(data_tot,20)
+data_tot_top <- head(data_tot,12)
 ggplot(data_tot_top, aes(x =process, y = total.pct , fill = self.pct/total.pct)) + geom_bar(stat="identity") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ggtitle('By parent process coloured by self portion')
-ggsave("parentColByPortion.pdf",
-    width = 10,
-    height = 8,
-    dpi = 1200)
+ggsave("parentColByPortion.pdf")
 unlink(tmp)
 ## =============================================================================
