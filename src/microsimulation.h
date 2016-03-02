e2860a81813cacdf175f4eb95237064ee4956bb5
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-03-02 15:57:22 +0100

    Documentation of the rllogis_trunc function

--- src/microsimulation.h
+++ src/microsimulation.h
@@ -1018,8 +1018,14 @@ namespace R {
 
   /**
      @brief rllogis function for a random covariate from a log-logistic distribution with shape and scale.
+     S(t) = 1/(1+(t/scale)^shape).
   */
   double rllogis(double shape, double scale);
+  /**
+     @brief rllogis_trunc function for a random covariate from a log-logistic distribution with shape and scale
+     with minimum time left.
+     S(t|t>x)=S(t)/S(x) where S(t)=1/(1+(t/scale)^shape).
+  */
   double rllogis_trunc(double shape, double scale, double left);
 
 }
