bbb77d1d6043a8e107e56a4f327eeaa07b25cec9
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-07-23 14:12:33 +0200

    Re-celebrating gleason prop after table bug-fix
    Only a small difference after bug-fix

--- R/rcpp_hello_world.R
+++ R/rcpp_hello_world.R
@@ -188,10 +188,10 @@ FhcrcParameters <- list(
     sebeta2=c(0.0913,0.3968, 0.0), # base::grade
     rev_mubeta2=c(0.051, 0.129, 0.1678), # ext::grade
     rev_sebeta2=c(0.064, 0.087, 0.3968), # ext::grade
-    alpha7=0.01259131,
-    beta7=0.01582902,
-    alpha8=-4.60448950,
-    beta8=0.08934935,
+    alpha7=0.01321145,
+    beta7=0.01577244,
+    alpha8=-4.60488625,
+    beta8=0.08851268,
     ## mubeta2.scale=1.0, # cf. 2.1
     ## beta.rho=0.62,
     c_txlt_interaction = 1.0,
