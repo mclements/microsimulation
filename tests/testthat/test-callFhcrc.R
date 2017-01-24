48012e653cc94bf727e933c7e4da8f9407352b6d
andreasakarlsson <andreas.a.karlsson@gmail.com>
2017-01-24 14:35:51 +0100

    Nested unit tests for all scenarios

--- tests/testthat/test-callFhcrc.R
+++ tests/testthat/test-callFhcrc.R
@@ -6,56 +6,46 @@ context("callFhcrc")
 ## or (faster)
 ## test_dir("/home/andkar/src/ki/microsimulation/tests/.")
 
-test_that("Check the structure of the returned fhcrc object", {
-    expect_is(sim <- callFhcrc(), "fhcrc")
-    expect_output(str(sim), "List of 13")
-    expect_output(str(sim), "$ n",                     fixed = TRUE)
-    expect_output(str(sim), "$ screen",                fixed = TRUE)
-    expect_output(str(sim), "$ enum",                  fixed = TRUE)
-    expect_output(str(sim), "$ lifeHistories",         fixed = TRUE)
-    expect_output(str(sim), "$ parameters",            fixed = TRUE)
-    expect_output(str(sim), "$ summary",               fixed = TRUE)
-    expect_output(str(sim), "$ healthsector.costs",    fixed = TRUE)
-    expect_output(str(sim), "$ societal.costs",        fixed = TRUE)
-    expect_output(str(sim), "$ psarecord",             fixed = TRUE)
-    expect_output(str(sim), "$ diagnoses",             fixed = TRUE)
-    expect_output(str(sim), "$ cohort",                fixed = TRUE)
-    expect_output(str(sim), "$ simulation.parameters", fixed = TRUE)
-    expect_output(str(sim), "$ falsePositives",        fixed = TRUE)
-})
+test_returned_object_structure <- function(obj = obj) {
+    test_that(paste("Check the structure of the returned fhcrc object with scenario:", try(obj$screen)), {
+        expect_is(obj, "fhcrc")
+        expect_output(str(obj), "List of 13")
+        expect_output(str(obj), "$ n",                     fixed = TRUE)
+        expect_output(str(obj), "$ screen",                fixed = TRUE)
+        expect_output(str(obj), "$ enum",                  fixed = TRUE)
+        expect_output(str(obj), "$ lifeHistories",         fixed = TRUE)
+        expect_output(str(obj), "$ parameters",            fixed = TRUE)
+        expect_output(str(obj), "$ summary",               fixed = TRUE)
+        expect_output(str(obj), "$ healthsector.costs",    fixed = TRUE)
+        expect_output(str(obj), "$ societal.costs",        fixed = TRUE)
+        expect_output(str(obj), "$ psarecord",             fixed = TRUE)
+        expect_output(str(obj), "$ diagnoses",             fixed = TRUE)
+        expect_output(str(obj), "$ cohort",                fixed = TRUE)
+        expect_output(str(obj), "$ simulation.parameters", fixed = TRUE)
+        expect_output(str(obj), "$ falsePositives",        fixed = TRUE)
+    })
+}
 
+test_scenario <- function(screen){
+    test_that(paste("Check microsimulation scenario:", screen), {
 
-test_that("Check that all scenarios run", {
+        ## N.b. double negation expect_failure() expect_error() <=> expect no error
+        expect_failure(expect_error(sim <- callFhcrc(screen = screen)))
+        ## Nested check of return object for each scenario
+        test_returned_object_structure(sim)
 
-    ## scenarios <-  c("noScreening", "randomScreen50to70", "twoYearlyScreen50to70", "fourYearlyScreen50to70", "screen50", "screen60", "screen70", "screenUptake", "stockholm3_goteborg", "stockholm3_risk_stratified", "goteborg", "risk_stratified", "mixed_screening", "regular_screen", "single_screen")
+    })
+}
 
-  ## ## This does not work as expected
-  ## ## http://stackoverflow.com/questions/10826365/how-to-test-that-an-error-does-not-occur
-  ## expect_error(for (screen in scenarios){expect_error(callFhcrc(), NA, info = paste("screen =", screen))})
+scenarios <-  c("noScreening", "randomScreen50to70", "twoYearlyScreen50to70", "fourYearlyScreen50to70", "screen50", "screen60", "screen70", "screenUptake", "stockholm3_goteborg", "stockholm3_risk_stratified", "goteborg", "risk_stratified", "mixed_screening", "regular_screen", "single_screen")
 
-  ## expect_error(lapply(c("noScreening", "screen500"), function(x) callFhcrc(screen = x)))
+lapply(scenarios, function(x) test_scenario(screen = x))
 
-  expect_failure(expect_error(callFhcrc(screen = "noScreening")))
-  expect_failure(expect_error(callFhcrc(screen = "randomScreen50to70")))
-  expect_failure(expect_error(callFhcrc(screen = "twoYearlyScreen50to70")))
-  expect_failure(expect_error(callFhcrc(screen = "fourYearlyScreen50to70")))
-  expect_failure(expect_error(callFhcrc(screen = "screen50")))
-  expect_failure(expect_error(callFhcrc(screen = "screen60")))
-  expect_failure(expect_error(callFhcrc(screen = "screen70")))
-  expect_failure(expect_error(callFhcrc(screen = "screenUptake")))
-  expect_failure(expect_error(callFhcrc(screen = "stockholm3_goteborg")))
-  expect_failure(expect_error(callFhcrc(screen = "stockholm3_risk_stratified")))
-  expect_failure(expect_error(callFhcrc(screen = "goteborg")))
-  expect_failure(expect_error(callFhcrc(screen = "risk_stratified")))
-  expect_failure(expect_error(callFhcrc(screen = "mixed_screening")))
-  expect_failure(expect_error(callFhcrc(screen = "regular_screen")))
-  expect_failure(expect_error(callFhcrc(screen = "single_screen")))
-
-})
 
 test_that("Check that input on the R side variables are equal on the C++ side", {
-# TBA
+    ## TBA
 })
 
+## WIP
 ## expect_output(print(sim),
 ##               "FHCRC prostate cancer model with 10 individual(s) under scenario 'noScreening'.")
