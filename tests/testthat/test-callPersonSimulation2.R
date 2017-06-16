359195fc424119fda71c51eae8d9e02fffa752bb
andreasakarlsson <andreas.a.karlsson@gmail.com>
2017-06-16 18:22:55 +0200

    Unit tests adapted to microsimulation core
    Simple unit tests for the callSimplePerson2() & callIllnessDeath() functions.

---
+++ tests/testthat/test-callPersonSimulation2.R
@@ -0,0 +1,49 @@
+library(microsimulation)
+context("callSimplePerson2")
+
+## To run all the test either:
+## R CMD check
+## or (faster)
+## test_dir("/home/andkar/src/ki/microsimulation/tests/.")
+
+test_returned_object_structure <- function(obj = obj) {
+    test_that(paste("Check the structure of the Illness-Death object:", try(obj$screen)), {
+        expect_is(obj, "list")
+        expect_output(str(obj), "List of 4")
+        expect_output(str(obj), "$ pt",                      fixed = TRUE)
+        expect_output(str(obj), "$ ut",                      fixed = TRUE)
+        expect_output(str(obj), "$ events",                  fixed = TRUE)
+        expect_output(str(obj), "$ prev",                    fixed = TRUE)
+    })
+}
+
+test_speed <- function(time_str){
+    test_that("Check that the execution speed was not doubled", {
+        ## As adviced we skip timing check on CRAN:
+        ## http://r-pkgs.had.co.nz/tests.html
+        ## To run locally: Sys.setenv(NOT_CRAN='true')
+        skip_on_cran()
+
+        ## Cut-of value arbitarily set. Note that this could fail on
+        ## really slow systems.
+        expect_true(time_str[2] <  0.1)
+    })
+}
+
+test_callSimplePerson2 <- function(){
+    test_that("Check Illness-Death model:", {
+
+        ## Make sure no errors are returned. N.b. double negation
+        ## expect_failure() expect_error() <=> expect no error
+        expect_failure(expect_error(time_str <- system.time(
+                                        sim <- callSimplePerson2())))
+
+        ## Check return object
+        test_returned_object_structure(sim)
+
+        ## Nested check of execution time
+        test_speed(time_str)
+    })
+}
+
+test_callSimplePerson2()
