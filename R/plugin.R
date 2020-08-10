aae8366aea20e3c0c9566306589765e109a085e9
Mark Clements <mark.clements@ki.se>
2020-08-10 16:47:47 +0200

    Update inline plugin and test

--- R/plugin.R
+++ R/plugin.R
@@ -1,8 +1,16 @@
+
+
+.microsimulationLdFlags <- function(){
+    paste( '-L"', system.file( "lib", package = "microsimulation" ), '" -lmicrosimulation',
+          sep = "" )
+}
+
 inlineCxxPlugin <- function(...) {
     ismacos <- Sys.info()[["sysname"]] == "Darwin"
     openmpflag <- if (ismacos) "" else "$(SHLIB_OPENMP_CFLAGS)"
     plugin <- Rcpp::Rcpp.plugin.maker(include.before = "#include <microsimulation.h>",
                                       libs           = paste(openmpflag,
+                                                             .microsimulationLdFlags(),
                                                              "$(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)"),
                                       package        = "microsimulation")
     settings <- plugin()
--- test/test_microsimulation.R
+++ test/test_microsimulation.R
@@ -2,6 +2,81 @@
 ## require(microsimulation)
 ## microsimulation:::.testPackage()
 
+## test simple
+library(microsimulation)
+microsimulation:::callSimplePerson()
+
+## test inline facilities
+library(Rcpp)
+library(microsimulation)
+sourceCpp(code="
+  //[[Rcpp::depends(microsimulation)]]
+  #include <microsimulation.h>
+  enum state_t {Healthy,Cancer,Death};
+  enum event_t {toOtherDeath, toCancer, toCancerDeath};
+  typedef std::map<std::string, std::vector<double> > Report;
+  class SimplePerson : public ssim::cProcess
+  {
+  public:
+    int id;
+    state_t state;
+    Report report;
+    SimplePerson() : id(-1) {};
+    void init();
+    virtual void handleMessage(const ssim::cMessage* msg);
+    void reporting(string name, double value);
+  };
+  /**
+      Initialise a simulation run for an individual
+  */
+  void SimplePerson::init() {
+    id++;
+    state = Healthy;
+    double tm = R::rweibull(8.0,85.0);
+    scheduleAt(tm,toOtherDeath);
+    scheduleAt(R::rweibull(3.0,90.0),toCancer);
+  }
+  void SimplePerson::reporting(std::string name, double value)  {
+    report[name].push_back(value);
+  }
+  /**
+      Handle receiving self-messages
+  */
+  void SimplePerson::handleMessage(const ssim::cMessage* msg) {
+    reporting(\"id\", double(id));
+    reporting(\"startTime\", previousEventTime);
+    reporting(\"endtime\", ssim::now());
+    reporting(\"state\", double(state));
+    reporting(\"event\", double(msg->kind));
+    switch(msg->kind) {
+    case toOtherDeath:
+    case toCancerDeath:
+      ssim::Sim::stop_simulation();
+      break;
+    case toCancer:
+      state = Cancer;
+      if (R::runif(0.0,1.0) < 0.5)
+	scheduleAt(ssim::now() + R::rweibull(2.0,10.0), toCancerDeath);
+      break;
+    default:
+      REprintf(\"No valid kind of event\\n\");
+      break;
+    } // switch
+    if (id % 10000 == 0) Rcpp::checkUserInterrupt();
+  } // handleMessage()
+  //[[Rcpp::export]]
+  Rcpp::List callSimplePerson(int n = 10) {
+    SimplePerson person;
+    Rcpp::RNGScope scope;
+    for (int i = 0; i < n; i++) {
+      ssim::Sim::create_process(&person);
+      ssim::Sim::run_simulation();
+      ssim::Sim::clear();
+    }
+    return Rcpp::wrap(person.report);
+  }")
+as.data.frame(callSimplePerson())
+
 ## 9013 bug
 require(microsimulation)
 debug(callFhcrc)
